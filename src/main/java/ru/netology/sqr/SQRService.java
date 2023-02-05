cba1dc94468480d403b8f7e9cd495f928a5f19b6
Arvo <arvofinland@gmail.com>
2023-02-05 14:35:05 +0300

    make a calc method

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,17 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int countSquares(int lowLimit, int highLimit) {
+
+        int counter = 0;
+        for (int i = 10; i <= 99; i++) {
+            int iSquare = i * i;
+            if (iSquare >= lowLimit) {
+                if (iSquare <= highLimit) {
+                  counter++;
+                }
+            }
+        }
+        return counter;
+    }
+}
