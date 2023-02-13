f40652ace8939a625971a4befa22e02113afe155
Arvo <arvofinland@gmail.com>
2023-02-13 22:31:26 +0300

    add new tests

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,10 +7,34 @@ public class SQRServiceTest {
     @Test
 
     public void calcWhenFewSquaresFound() {
-         SQRService service = new SQRService();
+        SQRService service = new SQRService();
 
-        int actual = service.countSquares( lowLimit: 200, highLimit: 300);
+
+        int actual = service.countSquares(200, 300);
         int expected = 3;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+
+    public void calcWhenFewSquaresFound2() {
+        SQRService service = new SQRService();
+
+
+        int actual = service.countSquares(300, 400);
+        int expected = 3;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void calcWhenFewSquaresFound3() {
+        SQRService service = new SQRService();
+
+
+        int actual = service.countSquares(50, 200);
+        int expected = 5;
 
         assertEquals(expected, actual);
     }
